9c01902080c5254ca6cf2be419616acf8c8c4cec
宏行 <63795522+soudan@users.noreply.github.com>
2020-05-29 13:06:25 +0900

    Add files via upload
    if test check

---
+++ Reigai1.java
@@ -0,0 +1,23 @@
+import javax.security.auth.kerberos.KeyTab;
+
+public class Reigai1 {
+    public static void main (String[]agres){
+        System.out.println("start in jabu");
+        int d =210;
+        
+                int f=d*9;
+                System.out.println(f);
+                if(f<=2000){
+                    
+                    System.out.println("A");
+                }else{
+                    System.out.println("B");                
+
+                }
+
+        
+    
+        
+    }
+    
+}
