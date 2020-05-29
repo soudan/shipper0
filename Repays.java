2e081bcac8a50ce4dce7001b7f565ece9bdc63c4
宏行 <63795522+soudan@users.noreply.github.com>
2020-05-29 13:13:03 +0900

    Add files via upload
    scan test 1to2end moved test

---
+++ Repays.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Repays {
+    public static void main(String[]agrs){
+
+System.out.println("SYSTEM STR");
+Scanner bon = new  Scanner(System.in);
+
+String guu=bon.nextLine();
+System.out.println("/" + guu);
+String uug=bon.nextLine();
+System.out.println("secand");
+System.out.println("//" + uug);
+bon.close();
+
+}
+
+}
