18c1feecc1381f4a2ae0dae419fcbe828216c706
宏行 <63795522+soudan@users.noreply.github.com>
2020-05-29 13:27:39 +0900

    Add files via upload
    set time 時間表示

---
+++ Tokimi.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+public class Tokimi{
+public static void main(String[]args){
+
+LocalDate tim = LocalDate.now();
+System.out.println(tim.toString());  
+}
+}
---
+++ Tokimi9.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+public class Tokimi9{
+public static void main(String[]args){
+
+LocalDate jika = LocalDate.now();
+
+
+System.out.println(jika.getYear()+"”N");
+System.out.println(jika.getMonthValue()+"ŒŽ" + jika.getDayOfMonth() + "“ú");
+}
+}
