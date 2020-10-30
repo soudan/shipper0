9d250560c1cec858a1ce066d374bd22e783f5d75
宏行 <63795522+soudan@users.noreply.github.com>
2020-10-30 16:38:49 +0900

    Add files via upload
    for double loop demos

---
+++ Adf.java
@@ -0,0 +1,17 @@
+public class Adf{
+public static void main(String[]agrs){
+	
+	for(int a=11;a>-1; --a){
+		System.out.println(a);
+	}
+	for(int s=188;s>-1;--s){
+		
+		for(int f=188;f>-1;f--){
+System.out.println(f+s);
+System.out.println("<<<");
+			
+		}
+		
+	}
+}
+}
